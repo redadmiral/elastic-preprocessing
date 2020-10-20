7b49be249b65ae3f0931848f32670689ed2d112c
Marco Lehner <marco-lehner@posteo.net>
2020-10-20 22:50:13 +0200

    :sparkle: sanity checks and fixed paths

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
+from decouple import config, UndefinedValueError
+
+test: str = "test_"
+
+try:
+    if config("PRODUCTION") == True:
+        test = ""
+except UndefinedValueError:
+    raise UndefinedValueError("Please define an environment variable 'PRODUCTION' with value True or False. If set to true production indices of elastic search will be overwritten.")
+
 ## elastic search credentials
 ELASTIC_URL = "141.13.94.164"
 ELASTIC_PORT = 9200
+
+# check for production env to avoid overwriting es indices
 
 # all file paths from this elastic-pp directory
 
@@ -8,7 +20,7 @@ ELASTIC_PORT = 9200
 NECKAR_PATH = "data/neckar.json"
 RANK_PATH = "data/response_pr.csv"
 EMBEDDINGS_PATH = "data/wikidata_translation_v1.tsv.gz"
-ALTLABELS_PATH = "data/response_altlabels.csv"
+ALTLABELS_PATH = "data/response_altLabels.csv"
 
 # path to output files
 NECKAR_FILTERED_PATH = "data/neckar_filtered.csv"
@@ -23,7 +35,7 @@ EMBEDDINGS_KEYS = ["embedding"]
 ALTLABELS_KEYS = ["altlabel", "wd_url"]
 
 # names of indices
-EMBEDDING_INDEX_NAME = "embedding_index"
-PR_INDEX_NAME = "pr_index"
-NECKAR_INDEX_NAME = "neckar_index"
-ALTLABELS_INDEX_NAME = "altlabel_index"
+EMBEDDING_INDEX_NAME = test + "embedding_index"
+PR_INDEX_NAME = test + "pr_index"
+NECKAR_INDEX_NAME = test + "neckar_index"
+ALTLABELS_INDEX_NAME = test + "altlabel_index"
--- create_index.py
+++ create_index.py
@@ -14,20 +14,21 @@ neckar: pd.DataFrame = pd.DataFrame()
 
 total_lines: int = 1200000
 linecount: int = 0
-print("Load NECKar dataset...")
+print("Load NECKar dataset...", flush=True)
 with open(config.NECKAR_PATH) as f:
     for line in f:
         if linecount % 10000 == 0:
-            print("Loading NECKar dataset line " + str(linecount) + " (" + str(round(linecount/total_lines*100, 2)) + "%).")
+            print("Loading NECKar dataset line " + str(linecount) + " (" + str(round(linecount/total_lines*100, 2)) + "%).", flush=True)
+        linecount = linecount + 1
         js: Dict = json.loads(line)
         df = [[("").join(["http://www.wikidata.org/entity/", js["WD_id"]]), js["WP_id_URL"], js["dbpedia_URL"], js["label"], js["neClass"]]]
         neckar = neckar.append(df)
 
-print("Remove duplicate entries...")
+print("Remove duplicate entries...", flush=True)
 neckar.columns = ["wd_url", "wp_url", "dbp_id", "label", "neClass"]
 neckar.to_csv(config.NECKAR_FILTERED_PATH)
 
-print("Create neckar_index...")
+print("Create neckar_index...", flush=True)
 
 helpers.check_and_delete(config.NECKAR_INDEX_NAME, elastic)
 
@@ -39,43 +40,42 @@ print(legit_ids)
 #res = elastic.get(index="neckar_index", id = "Q100")
 #print(res["_source"])
 
-print("Load PageRank data...")
+print("Load PageRank data...", flush=True)
 with open(config.RANK_PATH) as f:
     pr = pd.read_csv(f)
 
-pr = pr.drop(columns="label")
 pr.columns = ["wd_url", "pr"]
 
-print("Delete everything that's not PER, LOC or ORG...")
+print("Delete everything that's not PER, LOC or ORG...", flush=True)
 pr = pr[pr["wd_url"].isin(legit_ids)]
 
-print("Write PageRank index as CSV...")
+print("Write PageRank index as CSV...", flush=True)
 pr.to_csv(config.RANK_FILTERED_PATH)
 
 
-print("Create PR index...")
+print("Create PR index...", flush=True)
 helpers.check_and_delete(config.PR_INDEX_NAME, elastic)
 eshelp.bulk(elastic, helpers.doc_generator(pr, config.PR_INDEX_NAME, config.PR_KEYS))
 
-print("Load Altlabel data...")
+print("Load Altlabel data...", flush=True)
 with open(config.ALTLABELS_PATH) as f:
     labels = pd.read_csv(f)
 
 labels.columns = ["wd_url", "altlabel"]
 
-print("Delete everything that's not PER, LOC or ORG...")
+print("Delete everything that's not PER, LOC or ORG...", flush=True)
 labels = labels[labels["wd_url"].isin(legit_ids)]
 
-print("Write AltLabels index as CSV...")
+print("Write AltLabels index as CSV...", flush=True)
 labels.to_csv(config.ALTLABELS_FILTERED_PATH)
 
 
-print("Create AltLabel index...")
+print("Create AltLabel index...", flush=True)
 helpers.check_and_delete(config.ALTLABELS_INDEX_NAME, elastic)
-eshelp.bulk(elastic, helpers.doc_generator(labels, config.ALTLABELS_INDEX_NAME, config.ALTLABELS_INDEX_NAME))
+eshelp.bulk(elastic, helpers.doc_generator(labels, config.ALTLABELS_INDEX_NAME, config.ALTLABELS_KEYS))
 
 
-print("Load Embeddings...")
+print("Load Embeddings...", flush=True)
 embeddings = pd.DataFrame()
 
 with io.TextIOWrapper(io.BufferedReader(gzip.open(config.EMBEDDINGS_PATH))) as file:
@@ -83,14 +83,14 @@ with io.TextIOWrapper(io.BufferedReader(gzip.open(config.EMBEDDINGS_PATH))) as f
         line_parts: List[str] = line.split("\t")
         wd_url: str = line_parts[0][1:-1]
         if wd_url in legit_ids:
-            print(wd_url)
             embedding: List[float] = [float(i) for i in line_parts[1:]]
             embeddings = embeddings.append([[wd_url, embedding]])
+
 embeddings.columns = ["wd_url", "embedding"]
 
-print("Create embedding index...")
+print("Create embedding index...", flush=True)
 
 helpers.check_and_delete(config.EMBEDDING_INDEX_NAME, elastic)
 eshelp.bulk(elastic, helpers.doc_generator(embeddings, config.EMBEDDING_INDEX_NAME, config.EMBEDDINGS_KEYS))
 
-print("Indices created successfully.")
+print("Indices created successfully.", flush=True)
