886a7a7ecea1e5175507e50831179af7f4fb907a
Marco Lehner <marco-lehner@posteo.net>
2020-11-26 18:27:27 +0100

    nach mir die Sintflut

--- download_gerbil_stuff.sh
+++ download_gerbil_stuff.sh
@@ -1,4 +1,4 @@
-bzip2 -d < curl http://downloads.dbpedia.org/2016-10/core-i18n/de/redirects_de.ttl.bz2 > redirects_de.ttl
-bzip2 -d < curlhttp://downloads.dbpedia.org/2016-10/core-i18n/de/labels_de.ttl.bz2 > labels_de.ttl
+curl http://downloads.dbpedia.org/2016-10/core-i18n/de/redirects_de.ttl.bz2 | bzip2 -d > redirects_de.ttl
+curl http://downloads.dbpedia.org/2016-10/core-i18n/de/labels_de.ttl.bz2 | bzip2 -d > labels_de.ttl
 
 
