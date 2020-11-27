c19d55f856963963550f07e96d185255197d30ba
Marco Lehner <marco-lehner@posteo.net>
2020-11-27 17:18:58 +0100

    typeerror fixed

--- load_gerbil_data.py
+++ load_gerbil_data.py
@@ -6,7 +6,7 @@ import decouple
 import os
 import pandas as pd
 from typing import List
-
+import time
 elastic = es.Elasticsearch([{"host": config.ELASTIC_URL, "port": config.ELASTIC_PORT}])
 
 if decouple.config("PRODUCTION") == "True":
@@ -16,13 +16,13 @@ else:
     print("Write to test indices.", flush=True)
     test = "test_"
 
-print("Delete NaN rows in column label.", flush=True)
+print(time.ctime() + ": Delete rows with NA value in column label.", flush=True)
 file = "data/gerbil_index.csv"
 index_name = test + "gerbil_dbpsg200"
 csv = pd.read_csv(file)
 csv = csv[(csv["label"].notna())]
 
-print("Create index " + index_name + " on " + config.ELASTIC_URL + ":" + config.ELASTIC_PORT, flush=True)
+print(time.ctime() + "Create index " + index_name + " on " + config.ELASTIC_URL + ":" + str(config.ELASTIC_PORT), flush=True)
 if "embedding" in csv.columns:
     helpers.check_and_delete(index_name, elastic)
     eshelp.bulk(elastic, helpers.doc_generator(csv, index_name))
