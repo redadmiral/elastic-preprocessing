52eeff39ec4ac53d3bab92f28314a73f894152a0
Marco Lehner <marco-lehner@posteo.net>
2020-11-27 15:53:14 +0100

    :zap: Used pandas optimization.

--- load_gerbil_data.py
+++ load_gerbil_data.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import gensim
+import decouple
+import time
+
+if decouple.config("PRODUCTION") == "True":
+    print("Write to production indices.", flush=True)
+    test = ""
+else:
+    print("Write to test indices.", flush=True)
+    test = "_head"
 
 ## LABELS
-
-labels = pd.read_csv("data/labels_de.ttl", sep = " ", skiprows=1)
+print(time.ctime() + ": Get labels...", flush=True)
+labels = pd.read_csv("data/labels_de" + test + ".ttl", sep = " ", skiprows=1)
 labels.columns = ["de_url", "pred", "label", "dot"]
 labels = labels[["de_url", "label"]]
 labels["de_url"] = labels["de_url"].str.replace("<", "")
@@ -12,8 +21,8 @@ labels["de_url"] = labels["de_url"].str.replace(">", "")
 labels["label"] = labels["label"].str.replace("@de", "")
 
 ## GET ALTLABELS
-
-altlabels = pd.read_csv("data/redirects_de.ttl", sep = " ", skiprows=1)
+print(time.ctime() + ": Get altlabels...", flush=True)
+altlabels = pd.read_csv("data/redirects_de" + test + ".ttl", sep = " ", skiprows=1)
 altlabels.columns = ["label", "pred", "de_url", "dot"]
 altlabels = altlabels[["de_url", "label"]]
 altlabels["de_url"] = altlabels["de_url"].str.replace("<", "").str.replace(">", "")
@@ -21,33 +30,31 @@ altlabels["label"] = altlabels["label"].str.split("/").str[-1].str.replace(">", 
 altlabels["label"] = altlabels["label"].str.replace("_", " ")
 
 labels = labels.append(altlabels)
-
+print(time.ctime() + ": Merge language links...", flush=True)
 ## MERGE WITH LANGUAGE LINKS
-langlinks = pd.read_csv("data/interlinks_de.ttl", sep = " ", skiprows=1)
+langlinks = pd.read_csv("data/interlinks_de" + test + ".ttl", sep = " ", skiprows=1)
 langlinks.columns = ["de_url", "pred", "url", "dot"]
 langlinks = langlinks[["de_url", "url"]]
 langlinks["de_url"] = langlinks["de_url"].str.replace("<", "").str.replace(">", "")
 langlinks["url"] = langlinks["url"].str.replace("<", "").str.replace(">", "")
-labels = labels.merge(langlinks)
+labels = labels.merge(langlinks, how="outer")
 
+print(time.ctime() + ": Load vectors...", flush=True)
 ## LOAD VECTORS
-def get_vector(url: str, vectors: gensim.models.KeyedVectors):
-    try:
-        response = vectors.get_vector(url)
-    except KeyError:
-        response = None
-    return response
-
 keyed_vectors = gensim.models.KeyedVectors.load("data/dbpedia_500_4_sg_200_vectors.kv")
+vectors = keyed_vectors.vectors
+urls = keyed_vectors.index2word
 
-result = pd.DataFrame()
+print(time.ctime() + ": Flatten and create DF...", flush=True)
+vectors = list(map(str, vectors.tolist()))
+#vectors = np.genfromtxt("data/vectorshead.csv", delimiter = ", ")
+#urls = list(pd.read_csv("data/urlshead.csv", header=None)[0])
 
-for row in labels.iterrows():
-    try:
-        id = ":".join(["dbr", row[1]["url"].split("/")[-1]])
-        embedding = keyed_vectors.get_vector(id)
-    except KeyError:
-        embedding = None
-    result = result.append([[row[1]["url"], row[1]["de_url"], row[1]["label"], embedding]])
+embeddings = pd.DataFrame(list(zip(urls, vectors)), columns=["url", "embedding"])
+embeddings["url"] = embeddings["url"].str.replace("dbr:", "http://dbpedia.org/resource/")
 
-result.to_csv("gerbil_index.csv")
+print(time.ctime() + ": Merge embeddings with labels...", flush=True)
+labels = labels.merge(embeddings, how="outer")
+
+print(time.ctime() + ": Write to disk...", flush=True)
+labels.to_csv("gerbil_index.csv")
