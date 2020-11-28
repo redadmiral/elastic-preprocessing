61e1bb40aef38fd4440ad54e166159cdc4855cb7
Marco Lehner <marco-lehner@posteo.net>
2020-11-28 15:36:46 +0100

    enable inner join

--- helpers.py
+++ helpers.py
@@ -1,12 +1,13 @@
 import elasticsearch as es
 import warnings
 
+
 def doc_generator(df, index_name: str):
     df_iter = df.iterrows()
     for index, document in df_iter:
         yield {
                 "_index": index_name,
-                "_id" : f"{document['id']}",
+                "_id": f"{document['id']}",
                 "_source": {key: value for key, value in document.items()},
             }
 
--- parse_gerbil_data.py
+++ parse_gerbil_data.py
@@ -11,6 +11,8 @@ else:
     print("Write to test indices.", flush=True)
     test = "_head"
 
+mergestyle = "inner"
+
 ## LABELS
 print(time.ctime() + ": Get labels...", flush=True)
 labels = pd.read_csv("data/labels_de" + test + ".ttl", sep = " ", skiprows=1)
@@ -38,24 +40,24 @@ langlinks.columns = ["de_url", "pred", "url", "dot"]
 langlinks = langlinks[["de_url", "url"]]
 langlinks["de_url"] = langlinks["de_url"].str.replace("<", "").str.replace(">", "")
 langlinks["url"] = langlinks["url"].str.replace("<", "").str.replace(">", "")
-labels = labels.merge(langlinks, how="outer")
+labels = labels.merge(langlinks, how=mergestyle)
 
 print(time.ctime() + ": Load vectors...", flush=True)
 ## LOAD VECTORS
-#keyed_vectors = gensim.models.KeyedVectors.load("data/dbpedia_500_4_sg_200_vectors.kv")
-#vectors = keyed_vectors.vectors
-#urls = keyed_vectors.index2word
+keyed_vectors = gensim.models.KeyedVectors.load("data/dbpedia_500_4_sg_200_vectors.kv")
+vectors = keyed_vectors.vectors
+urls = keyed_vectors.index2word
 
 print(time.ctime() + ": Flatten and create DF...", flush=True)
-vectors = np.genfromtxt("data/vectorshead.csv", delimiter = ", ")
+#vectors = np.genfromtxt("data/vectorshead.csv", delimiter = ", ")
 vectors = list(map(str, vectors.tolist()))
-urls = list(pd.read_csv("data/urlshead.csv", header=None)[0])
+#urls = list(pd.read_csv("data/urlshead.csv", header=None)[0])
 
 embeddings = pd.DataFrame(list(zip(urls, vectors)), columns=["url", "embedding"])
 embeddings["url"] = embeddings["url"].str.replace("dbr:", "http://dbpedia.org/resource/")
 
 print(time.ctime() + ": Merge embeddings with labels...", flush=True)
-labels = labels.merge(embeddings, how="outer")
+labels = labels.merge(embeddings, how=mergestyle)
 
 print(time.ctime() + ": Write to disk...", flush=True)
-labels.to_csv("gerbil_index.csv")
+labels.to_csv("data/gerbil_index_" + mergestyle + ".csv")
